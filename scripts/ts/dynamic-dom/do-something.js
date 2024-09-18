e0f21aec58edd2c6acb4a9dbb2b5663671d54b2e
Sol Bae <b.seoyoung@wustl.edu>
2024-09-18 11:19:30 -0500

    committed

--- scripts/ts/dynamic-dom/do-something.js
+++ scripts/ts/dynamic-dom/do-something.js
@@ -1,3 +1,20 @@
 export function doSomething() {
   console.log("Put some code in here!");
+  setup();
+  doSomething();
+
+}
+
+export function setupSticky() {
+  window.onscroll = function() {myFunction()};
+    var header = document.getElementById("main-menu-container");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+     if (window.scrollY > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
 }
---
+++ scripts/ts/dynamic-dom/sticky.ts
@@ -0,0 +1,19 @@
+/*
+export function setUpSticky() { 
+
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("main-menu-container");
+
+    var sticky = header.offsetTop;
+
+    function myFunction() { 
+        if (window.pageYOffset > sticky) {
+            header?.classList.add("sticky");
+        } else { 
+            header?.classList.remove("sticky");
+
+        }
+    }
+}
+*/
